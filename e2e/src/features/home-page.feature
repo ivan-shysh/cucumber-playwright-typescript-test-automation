feecc153d452ca17f915e1cd6549c0993e290bfd
Ivan Shyshkevich <ishyshkevich@moderncampus.com>
2024-04-05 17:16:57 -0400

    added regex, cucumber parameters + comments to verify-element-visibility.ts and home-page.feature

--- e2e/src/features/home-page.feature
+++ e2e/src/features/home-page.feature
@@ -5,5 +5,5 @@ Feature: As a user I expect to be able to navigate to the home page
   @regression  #will likely be on every scenario, to run a full regression suite, either locally or in CI 
   Scenario: As a user I expect to be able to see contacts
     Given I am on the "home" page
-    Then the contacts header should contain the text Contacts
+    Then the "contacts header" should contain the text "Contacts"
 
--- e2e/src/step-definitions/assertions/verify-element-visibility.ts
+++ e2e/src/step-definitions/assertions/verify-element-visibility.ts
@@ -2,14 +2,14 @@ import { Then } from '@cucumber/cucumber'
 import { expect } from '@playwright/test'
 
 Then(
-    /^the contacts header should contain the text Contacts$/,
-    async function() {
+    /^the "([^"]*)" should contain the text "(.*)"$/, // adding regex that accepts 'something' in double quotes here as well for two paremeters passed via home-page.feature
+    async function(elementKey: string, expectedElementText: string) {
 
-        console.log("the contacts header should contain the text Contacts")
+        console.log(`the ${elementKey} should contain the text ${expectedElementText}`) 
 
         const content = await global.page.textContent("[data-id='contacts']")
 
-        expect(content).toBe('Contacts')
+        expect(content).toBe(expectedElementText) // that is the text that is defined by the Cucumber parameter in home-page.feature
 
     }
 )
