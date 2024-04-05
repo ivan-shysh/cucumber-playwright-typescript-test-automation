d7f99bd1ce7bb1f7fc174bdd7cdbe83c00cf6e72
Ivan Shyshkevich <ishyshkevich@moderncampus.com>
2024-04-05 16:50:04 -0400

    enabled parameter in navigation.ts step definition file + comments

--- e2e/src/step-definitions/navigation.ts
+++ e2e/src/step-definitions/navigation.ts
@@ -1,10 +1,10 @@
 import { Given } from '@cucumber/cucumber'
 
 Given(
-    /^I am on the home page$/,
-    async function() {
+    /^I am on the "([^"]*)" page$/, //we added a regex here that takes a string inside a set of double quotes "([^"]*)"
+    async function(pageId: string) { //we declare that the function takes one parameter here, the type of the parameter is string
 
-        console.log("I am on the home page");
+        console.log(`I am on the ${pageId} page`); // The console logs a message declaring the name of the page that is passed as a parameter in the home-page.feature
 
         await global.page.goto("http://localhost:3000/")
 
