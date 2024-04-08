cee201998057e185c514ce1a8d3aa4ae499c124d
Ivan Shyshkevich <ishyshkevich@moderncampus.com>
2024-04-08 14:48:38 -0400

    added video report folder generation capability in hooks.ts

--- e2e/src/step-definitions/hooks/hooks.ts
+++ e2e/src/step-definitions/hooks/hooks.ts
@@ -14,8 +14,13 @@ AfterAll(async() => {
 });
 
 // runs before every single scenario/test
-Before(async() => {
-    global.context = await global.browser.newContext(); // browser context is an isolated incognito-like session within the browser; each new scenario is recommended
+Before(async(scenario) => {
+    global.context = await global.browser.newContext({
+        recordVideo: {
+            dir: './reports/videos/' + scenario.pickle.name, 
+        }
+    }); 
+    // browser context is an isolated incognito-like session within the browser; each new scenario is recommended
     // to be run in a new browser context so the browser state is isolated between tests
     global.page = await global.context.newPage(); // page referes to a sinlge window or a pop-up page within a browser context; it is used to navigate to URL's and 
     // interact with the pages content; we want to open a new page at the start of each test
