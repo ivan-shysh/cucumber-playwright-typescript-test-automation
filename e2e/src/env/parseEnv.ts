6a1a2a284cd2b3f0ab75dc6ded31f2395c424854
Ivan Shyshkevich <ishyshkevich@moderncampus.com>
2024-04-29 15:24:18 -0400

    added e2e/src/env/parseEnv.ts

---
+++ e2e/src/env/parseEnv.ts
@@ -0,0 +1,9 @@
+//the purpose of this function we want to say if it passes through evn then we expect an environment varible to be set, otherwise stop the test - if no browsers
+//are defined, we can't run the tests or the browser doesn't exist
+export const env = (key: string): string => {
+    const value = process.env[key]
+    if(!value) {
+        throw Error(`No environment variable found for ${key}`)
+    }
+    return value;
+}
