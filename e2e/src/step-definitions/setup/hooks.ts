fdc6d9dcfb6bf5175d43a24bbcde4f4c29365e39
Ivan Shyshkevich <ishyshkevich@moderncampus.com>
2024-05-13 16:25:20 -0400

    huge refactoring of hooks.ts

--- e2e/src/step-definitions/setup/hooks.ts
+++ e2e/src/step-definitions/setup/hooks.ts
@@ -1,33 +1,27 @@
-import {BeforeAll, Before, AfterAll, After} from "@cucumber/cucumber";
-const {chromium} = require("playwright");
+import {Before, After, ITestCaseHookParameter} from "@cucumber/cucumber";
+import { ScenarioWorld } from './world'
 
-// runs before all of the tests
-BeforeAll(async() => {
-    global.browser = await chromium.launch({
-        headless:false,
-    })
-});
-
-// runs after all of the tests were completed - we clean up and close our browser after all our tests are complete
-AfterAll(async() => {
-    await global.browser.close();
-});
 
 // runs before every single scenario/test
-Before(async(scenario) => {
-    global.context = await global.browser.newContext({
+Before(async function (this: ScenarioWorld, scenario) {
+    console.log(`Running cucumber scenario ${scenario.pickle.name}`)
+
+    const contextOptions = {
         recordVideo: {
-            dir: './reports/videos/' + scenario.pickle.name, 
+            dir: './reports/videos/' + scenario.pickle.name,
         }
-    }); 
-    // browser context is an isolated incognito-like session within the browser; each new scenario is recommended
-    // to be run in a new browser context so the browser state is isolated between tests
-    global.page = await global.context.newPage(); // page referes to a sinlge window or a pop-up page within a browser context; it is used to navigate to URL's and 
-    // interact with the pages content; we want to open a new page at the start of each test
+    }
+    
+    const ready = await this.init(contextOptions)
+    return ready;
 });
 
 // runs after every test
-After(async(scenario) => {
+After(async function(this: ScenarioWorld, scenario) {
+
+    const {
+        screen: { page, browser}
+    } = this;
 
     const scenarioStatus = scenario.result?.status;
     // The optional chaining operator (?.) offers a safer and more concise way to access properties deep within object chains, 
@@ -35,11 +29,12 @@ After(async(scenario) => {
     // making code more robust and easier to read.
 
     if (scenarioStatus === 'FAILED') {
-        await global.page.screenshot({
+        await page.screenshot({
             path: `./reports/screenshots/${scenario.pickle.name}.png`
         });
     }
 
-    await global.page.close() // we want to clean up and close our page after each test
+    await browser.close();
+    return browser;
 });
 
