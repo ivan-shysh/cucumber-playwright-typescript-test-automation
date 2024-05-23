afbf7dee2ecf7489325a1576e3ec27a1034126e4
Ivan Shyshkevich <ishyshkevich@moderncampus.com>
2024-05-23 17:06:28 -0400

    added a comment on "this" binding in navigation.ts

--- e2e/src/step-definitions/navigation.ts
+++ e2e/src/step-definitions/navigation.ts
@@ -5,7 +5,7 @@ Given(
     async function(pageId: string) { //we declare that the function takes one parameter here, the type of the parameter is string
         const {
             screen: { page },
-        } = this;
+        } = this; // In Cucumber step definitions, 'this' is bound to the current instance fo the 'World' class
 
         console.log(`I am on the ${pageId} page`); // The console logs a message declaring the name of the page that is passed as a parameter in the home-page.feature
 
