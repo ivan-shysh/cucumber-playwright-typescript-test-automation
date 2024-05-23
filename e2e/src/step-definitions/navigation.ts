9925d265a69bcbeb72495aef7ee9d6cddfcf92ee
Ivan Shyshkevich <ishyshkevich@moderncampus.com>
2024-05-23 16:04:32 -0400

    modified navigation.ts by removing global object

--- e2e/src/step-definitions/navigation.ts
+++ e2e/src/step-definitions/navigation.ts
@@ -3,10 +3,13 @@ import { Given } from '@cucumber/cucumber'
 Given(
     /^I am on the "([^"]*)" page$/, //we added a regex here that takes a string inside a set of double quotes "([^"]*)"
     async function(pageId: string) { //we declare that the function takes one parameter here, the type of the parameter is string
+        const {
+            screen: { page },
+        } = this;
 
         console.log(`I am on the ${pageId} page`); // The console logs a message declaring the name of the page that is passed as a parameter in the home-page.feature
 
-        await global.page.goto("http://localhost:3000/")
+        await page.goto("http://localhost:3000/")
 
     }
 )
