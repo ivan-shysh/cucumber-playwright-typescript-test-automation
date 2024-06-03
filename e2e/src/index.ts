085e68d9e45a226ca4907afe6c4bade9dc5e18e3
Ivan Shyshkevich <ishyshkevich@moderncampus.com>
2024-06-03 17:08:28 -0400

    added definitions and export of tags in index.ts

--- e2e/src/index.ts
+++ e2e/src/index.ts
@@ -3,3 +3,9 @@ const common = `/src/features/**/*.feature \
                 --require ./src/step-definitions/**/**/.ts \
                 -f json:./reports/report.json \
                 --format progress-bar`;
+
+const dev = `${common} --tags '@dev'`;
+const smoke = `${common} --tags '@smoke'`;
+const regression = `${common} --tag '@regression'`;
+
+export {dev, smoke, regression}
