570529ffeb6e069a1d8e94306e3e7efa510ce63e
Ivan Shyshkevich <ishyshkevich@moderncampus.com>
2024-06-26 09:40:45 -0400

    added imports and dotenv config functions to cucumber-reporter.ts

--- e2e/src/reporter/cucumber-report.ts
+++ e2e/src/reporter/cucumber-report.ts
@@ -1,5 +1,8 @@
+import dotenv from 'dotenv'
 import reporter, { Options } from 'cucumber-html-reporter'
 import { env } from '../env/parseEnv'
+
+dotenv.config({path: env('COMMON_CONFIG_FILE')})
 
 const options: Options = {
     theme: 'bootstrap',
