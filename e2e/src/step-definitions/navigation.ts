3093820545e47d6da10574503071ec8c2faaaf02
Ivan Shyshkevich <ishyshkevich@moderncampus.com>
2024-06-28 17:38:02 -0400

    changed page type and page navigation function in navigation.ts

--- e2e/src/step-definitions/navigation.ts
+++ e2e/src/step-definitions/navigation.ts
@@ -1,16 +1,20 @@
 import { Given } from '@cucumber/cucumber';
-import { ScenarioWorld } from './setup/world';
+import { PageId } from '../env/global';
+import {
+    navigateToPage
+} from '';
 
 Given(
     /^I am on the "([^"]*)" page$/, //we added a regex here that takes a string inside a set of double quotes "([^"]*)"
-    async function(this: ScenarioWorld, pageId: string) { //we declare that the function takes one parameter here, the type of the parameter is string
+    async function(pageId: PageId) { //we declare that the function takes one parameter here, the type of the parameter is string
         const {
             screen: { page },
+            globalConfig,
         } = this; // In Cucumber step definitions, 'this' is bound to the current instance fo the 'World' class
 
         console.log(`I am on the ${pageId} page`); // The console logs a message declaring the name of the page that is passed as a parameter in the home-page.feature
 
-        await page.goto("http://localhost:3000/")
+        await navigateToPage(page, pageId, globalConfig)
 
     }
 )
