a6cd4312f0a53d350133f09c1695db6ec68c6457
Ivan Shyshkevich <ishyshkevich@moderncampus.com>
2024-06-28 17:58:05 -0400

    added a comment in run_tests.sh

--- e2e/run_tests.sh
+++ e2e/run_tests.sh
@@ -1,7 +1,7 @@
 #cucumber tag
 tag=$1
 
-export COMMON_CONFIG_FILE=env/common.env
+export COMMON_CONFIG_FILE=env/common.env #makes all the env vars available to all the processes that result from this script
 
 #run cucumber tests & on failure run postcucumber
 npm run cucumber -- --profile $tag || npm run postcucumber
