5967fc7217b68e2d717bf8ff163d8e7a6e8027eb
Ivan Shyshkevich <ishyshkevich@moderncampus.com>
2024-07-18 09:18:27 -0400

    added page elements mappings code in index.ts

--- e2e/src/index.ts
+++ e2e/src/index.ts
@@ -16,9 +16,19 @@ const mappingFiles = fs.readdirSync(`${process.cwd()}${env('PAGE_ELEMENTS_PATH')
 
 console.log(mappingFiles); // remove later 
 
+const pageElementMappings: PageElementMappings = mappingFiles.reduce(
+    (pageElementConfigAcc, file) => {
+        const key = file.replace('.json', '')
+        const elementMappings = getJsonFromFile(`${env('PAGE_ELEMENTS_PATH')}${file}`);
+        return {...pageElementConfigAcc, [key]: elementMappings}
+    },
+    {}
+ )
+
 const worldParameters: GlobalConfig = {
     hostsConfig,
-    pagesConfig
+    pagesConfig,
+    pageElementMappings
 }
 
 const common = `./src/features/**/*.feature \
@@ -34,8 +44,8 @@ const regression = `${common} --tags '@regression'`;
 
 export { dev, smoke, regression };
 
-//fs stands for the File System module in Node.js. 
-//It provides an API to interact with the file system, allowing to read, write, delete, and perform other file operations.
-//fs.readdirSync is a synchronous method provided by the fs module to read the contents of a directory. 
-//It returns an array of the names of the files and directories within the specified directory.
+/*fs stands for the File System module in Node.js. 
+  It provides an API to interact with the file system, allowing to read, write, delete, and perform other file operations.
+  fs.readdirSync is a synchronous method provided by the fs module to read the contents of a directory. 
+  It returns an array of the names of the files and directories within the specified directory. */
 
