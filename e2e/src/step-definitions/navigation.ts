c34cf8376ad4eb06783a808ce2d406239d3171ee
Ivan Shyshkevich <ishyshkevich@moderncampus.com>
2024-07-18 09:28:01 -0400

    added globalVariables to navigation.ts

--- e2e/src/step-definitions/navigation.ts
+++ e2e/src/step-definitions/navigation.ts
@@ -9,10 +9,13 @@ Given(
     async function(pageId: PageId) { //we declare that the function takes one parameter here, the type of the parameter is string
         const {
             screen: { page },
+            globalVariables,
             globalConfig,
         } = this; // In Cucumber step definitions, 'this' is bound to the current instance fo the 'World' class
 
         console.log(`I am on the ${pageId} page`); // The console logs a message declaring the name of the page that is passed as a parameter in the home-page.feature
+
+        globalVariables.currentScreen = pageId;
 
         await navigateToPage(page, pageId, globalConfig)
 
