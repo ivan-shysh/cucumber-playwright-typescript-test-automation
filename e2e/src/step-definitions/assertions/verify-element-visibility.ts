e68710e70c9262a3096e144028dbbc8ae1df1528
Ivan Shyshkevich <ishyshkevich@moderncampus.com>
2024-07-25 17:13:07 -0400

    refactored verify-element-visibility according to recent changes

--- e2e/src/step-definitions/assertions/verify-element-visibility.ts
+++ e2e/src/step-definitions/assertions/verify-element-visibility.ts
@@ -25,14 +25,18 @@ Then(
 
 Then(
     /^the "([^"]*)" should contain the text "(.*)"$/, // adding regex that accepts 'something' in double quotes here as well for two paremeters passed via home-page.feature
-    async function(this: ScenarioWorld, elementKey: string, expectedElementText: string) {
+    async function(this: ScenarioWorld, elementKey: ElementKey, expectedElementText: string) {
         const {
             screen: { page },
+            globalConfig,
+            globalVariables,
         } = this;
 
         console.log(`the ${elementKey} should contain the text ${expectedElementText}`) 
 
-        const content = await page.textContent("[data-id='contacts']")
+        const elementIdentifier = getElementLocator(page, elementKey, globalVariables, globalConfig)
+
+        const content = await page.textContent(elementIdentifier)
 
         expect(content).toBe(expectedElementText) // that is the text that is defined by the Cucumber parameter in home-page.feature
 
