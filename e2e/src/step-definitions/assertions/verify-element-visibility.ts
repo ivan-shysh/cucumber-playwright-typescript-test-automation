d39482082cfa6c8a9a1d7c2eedf426b3ce7709e4
Ivan Shyshkevich <ishyshkevich@moderncampus.com>
2024-08-14 14:35:55 -0400

    imported waitFor() function into verify-element-visibility.ts removed playwright/test from same file

--- e2e/src/step-definitions/assertions/verify-element-visibility.ts
+++ e2e/src/step-definitions/assertions/verify-element-visibility.ts
@@ -1,8 +1,8 @@
 import { Then } from '@cucumber/cucumber';
-import { expect } from '@playwright/test';
 import { ScenarioWorld } from '../setup/world';
 import { ElementKey } from '../../env/global';
 import { getElementLocator } from '../../support/web-element-helper'
+import { waitFor } from '../../support/wait-for-behavior';
 
 Then(
     /^the "([^"]*)" should be displayed$/,
