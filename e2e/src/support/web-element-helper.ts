d1ea0ca4a57168c4d87dd43ce23d9bf563f21ca4
Ivan Shyshkevich <ishyshkevich@moderncampus.com>
2024-09-09 17:47:47 -0400

    -m

--- e2e/src/support/web-element-helper.ts
+++ e2e/src/support/web-element-helper.ts
@@ -1,16 +1,13 @@
 import { Page } from 'playwright'
-import { ElementKey, ElementLocator, GlobalConfig, GlobalVariables } from '../env/global'
+import { ElementKey, ElementLocator, GlobalConfig } from '../env/global'
 
 export const getElementLocator = (
     page: Page, 
-    elementKey: ElementKey, 
-    globalVariables: GlobalVariables, 
+    elementKey: ElementKey,  
     globalConfig: GlobalConfig,
 ): ElementLocator => {
 
     const { pageElementMappings } = globalConfig;
-
-    const currentPage = globalVariables.currentScreen;
 
     return pageElementMappings[currentPage]?.[elementKey] || pageElementMappings.common?.[elementKey]
 }
