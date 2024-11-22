3c17a98c74991649cfe47a010553df01166a6f0c
Ivan Shyshkevich <ishyshkevich@moderncampus.com>
2024-11-21 20:00:27 -0500

    added step-definition for verify-element-checked.ts

--- e2e/src/step-definitions/assertions/verify-element-checked.ts
+++ e2e/src/step-definitions/assertions/verify-element-checked.ts
@@ -3,3 +3,22 @@ import { waitFor } from '../../support/wait-for-behavior';
 import { ScenarioWorld } from '../setup/world';
 import { getElementLocator } from '../../support/web-element-helper';
 import { ElementKey } from '../../env/global';
+
+Then(
+    /^the "([^"]*)" radio button should be checked$/,
+    async function(this: ScenarioWorld, elementKey: ElementKey) {
+        const {
+            screen: { page },
+            globalConfig,
+        } = this;
+
+        console.log(`the ${elementKey} radio button should be checked`);
+
+        const elementIdentifier = getElementLocator(page, elementKey, globalConfig);
+
+        await waitFor ( async () => { 
+            const isElementChecked = await page.isChecked(elementIdentifier)
+            return isElementChecked;
+        });
+    }
+)
