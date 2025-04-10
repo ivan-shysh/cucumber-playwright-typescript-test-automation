52e9f93a7faec1247ddc5df6081c91d81483470e
Ivan Shyshkevich <ishyshkevich@moderncampus.com>
2025-04-10 17:31:59 -0400

    added equal text step definition to verify-iframe.ts

--- e2e/src/step-definitions/assertions/verify-iframe.ts
+++ e2e/src/step-definitions/assertions/verify-iframe.ts
@@ -46,3 +46,24 @@ Then(
             })
         }
     ) 
+
+Then(
+    /^the "([^"]*)" on the "([^"]*)" iframe should( not)? equal the text "(.*)"$/,
+    async function(this: ScenarioWorld, elementKey: ElementKey, iframeName: string, negate: boolean, expectedElementText: string) {
+        const {
+            screen: { page },
+            globalConfig,
+        } = this;    
+
+        console.log(`the ${elementKey} should ${negate ? 'not' : ''} equal the text ${expectedElementText}`)        
+
+        const elementIdentifier = getElementLocator(page, elementKey, globalConfig);        
+        const iframeIdentifier = getElementLocator(page, iframeName, globalConfig);        
+        const elementIframe = await getIframeElement(page, iframeIdentifier);        
+        
+        await waitFor ( async () => {
+            const elementText = await elementIframe?.textContent(elementIdentifier)
+            return (elementText === expectedElementText) === !negate;
+        })
+    }
+)
