8922face07ec7a5ebaa9c8a70f0747b5979379bc
Ivan Shyshkevich <ishyshkevich@moderncampus.com>
2025-04-17 12:39:18 -0400

    aligned index.ts with original code

--- e2e/src/index.ts
+++ e2e/src/index.ts
@@ -3,18 +3,16 @@ import { env, getJsonFromFile } from './env/parseEnv'
 import {
     GlobalConfig,
     HostsConfig,
-    PagesConfig, 
+    PagesConfig,
     PageElementMappings,
-} from './env/global'
-import * as fs from "fs"
+} from './env/global';
+import * as fs from "fs";
 
 dotenv.config({path: env('COMMON_CONFIG_FILE')})
 
 const hostsConfig: HostsConfig = getJsonFromFile(env('HOSTS_URLS_PATH'));
-const pagesConfig: PagesConfig = getJsonFromFile(env('PAGES_URLS_PATH'));
-const mappingFiles = fs.readdirSync(`${process.cwd()}${env('PAGE_ELEMENTS_PATH')}`);
-
-console.log(mappingFiles); // remove later 
+const pagesConfig: PagesConfig = getJsonFromFile(env('PAGE_URLS_PATH'));
+const mappingFiles = fs.readdirSync(`${process.cwd()}${env('PAGE_ELEMENTS_PATH')}`)
 
 const pageElementMappings: PageElementMappings = mappingFiles.reduce(
     (pageElementConfigAcc, file) => {
@@ -23,31 +21,25 @@ const pageElementMappings: PageElementMappings = mappingFiles.reduce(
         return {...pageElementConfigAcc, [key]: elementMappings}
     },
     {}
- )
+)
 
 const worldParameters: GlobalConfig = {
     hostsConfig,
     pagesConfig,
-    pageElementMappings
-}
+    pageElementMappings,
+};
 
 const common = `./src/features/**/*.feature \
                 --require-module ts-node/register \
                 --require ./src/step-definitions/**/**/*.ts \
-                --world-parameters ${JSON.stringify(worldParameters)} \
                 -f json:./reports/report.json \
+                --world-parameters ${JSON.stringify(worldParameters)} \
                 --format progress-bar \
                 --parallel ${env('PARALLEL')} \
-                --retry ${env('RETRY')}`;
+                --retry ${env('RETRY')} `;
 
 const dev = `${common} --tags '@dev'`;
 const smoke = `${common} --tags '@smoke'`;
 const regression = `${common} --tags '@regression'`;
 
-export { dev, smoke, regression };
-
-/*fs stands for the File System module in Node.js. 
-  It provides an API to interact with the file system, allowing to read, write, delete, and perform other file operations.
-  fs.readdirSync is a synchronous method provided by the fs module to read the contents of a directory. 
-  It returns an array of the names of the files and directories within the specified directory. */
-
+export { dev, smoke, regression}
