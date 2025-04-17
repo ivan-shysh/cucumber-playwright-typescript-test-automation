3f22237728625ddece5c11776d6f34fb9508117d
Ivan Shyshkevich <ishyshkevich@moderncampus.com>
2025-04-17 13:13:11 -0400

    aligned web-element-helper.ts

--- e2e/src/support/web-element-helper.ts
+++ e2e/src/support/web-element-helper.ts
@@ -1,22 +1,16 @@
 import { Page } from 'playwright'
-import { ElementKey, ElementLocator, GlobalConfig } from '../env/global'
-import { getCurrentPageId } from './navigation-behavior';
+import { ElementKey, ElementLocator, GlobalConfig} from "../env/global";
+import {getCurrentPageId} from "./navigation-behavior";
 
 export const getElementLocator = (
-    page: Page, 
-    elementKey: ElementKey,  
+    page: Page,
+    elementKey: ElementKey,
     globalConfig: GlobalConfig,
 ): ElementLocator => {
 
-    const currentPage = getCurrentPageId(page, globalConfig)
+    const currentPage = getCurrentPageId(page, globalConfig);
 
     const { pageElementMappings } = globalConfig;
-
-    console.log('🌐 getElementLocator()');
-    console.log('🧭 Current page:', currentPage);
-    console.log('🔑 Element key:', elementKey);
-    console.log('📦 Page element mappings:', pageElementMappings[currentPage]);
-
 
     return pageElementMappings[currentPage]?.[elementKey] || pageElementMappings.common?.[elementKey]
 }
