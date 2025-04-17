283f464ecda0cff0dd9e3eeaa7bf9c09660b892b
Ivan Shyshkevich <ishyshkevich@moderncampus.com>
2025-04-17 12:57:55 -0400

    aligned world.ts and hooks.ts

--- e2e/src/step-definitions/setup/hooks.ts
+++ e2e/src/step-definitions/setup/hooks.ts
@@ -4,32 +4,25 @@ import { env, envNumber } from '../../env/parseEnv'
 
 setDefaultTimeout(envNumber('SCRIPT_TIMEOUT'));
 
-
-// runs before every single scenario/test
 Before(async function (this: ScenarioWorld, scenario) {
     console.log(`Running cucumber scenario ${scenario.pickle.name}`)
 
     const contextOptions = {
         recordVideo: {
-            dir: `${env('VIDEO_PATH')}${scenario.pickle.name}`,
+            dir : `${env('VIDEO_PATH')}${scenario.pickle.name}`,
         }
     }
-    
+
     const ready = await this.init(contextOptions)
-    return ready;
+    return ready
 });
 
-// runs after every test
 After(async function(this: ScenarioWorld, scenario) {
-
     const {
-        screen: { page, browser}
+        screen: { page, browser }
     } = this;
 
     const scenarioStatus = scenario.result?.status;
-    // The optional chaining operator (?.) offers a safer and more concise way to access properties deep within object chains, 
-    // especially when some parts of the chain may not exist. It prevents TypeErrors that could occur when accessing properties on undefined or null, 
-    // making code more robust and easier to read.
 
     if (scenarioStatus === 'FAILED') {
         const screenshot = await page.screenshot({
--- e2e/src/step-definitions/setup/world.ts
+++ e2e/src/step-definitions/setup/world.ts
@@ -7,23 +7,17 @@ import playwright, {
 } from "playwright";
 import { env } from '../../env/parseEnv'
 import { World, IWorldOptions, setWorldConstructor} from "@cucumber/cucumber";
-import { GlobalConfig } from "../../env/global";
+import { GlobalConfig } from '../../env/global'
 
-//Purpose: This part of the code is defining a type named Screen. 
-//This is not creating an object but specifying a blueprint for what an object of type Screen should look like. 
-//It's a contract or a template stating that any Screen object will have three properties: browser (of type Browser), 
-//context (of type BrowserContext), and page (of type Page). 
-//Each of these types (Browser, BrowserContext, and Page) are also defined by Playwright 
-//and represent different aspects of a browser automation setup.
 export type Screen = {
     browser: Browser;
     context: BrowserContext;
     page: Page;
-};
+}
 
 export class ScenarioWorld extends World {
-    constructor(options: IWorldOptions) { 
-        super(options);
+    constructor(options: IWorldOptions) {
+        super(options)
 
         this.globalConfig = options.parameters as GlobalConfig;
     }
@@ -31,47 +25,36 @@ export class ScenarioWorld extends World {
     globalConfig: GlobalConfig;
 
     screen!: Screen;
-// remove 
+
     async init(contextOptions?: BrowserContextOptions): Promise<Screen> {
         await this.screen?.page?.close();
-        await this.screen?.context?.close();
-        await this.screen?.browser?.close();
+        await this.screen?.context?.close()
+        await this.screen?.browser?.close()
 
-        const browser = await this.newBrowser(); /* We will create a custom function here that 
-        will determine our new browser based on an environment variable we set
-        and that environment variable will dictate if our tests run on Chromium, Firefox, WebKit */
-        const context = await browser.newContext({
-            ...(contextOptions || {}),
-            viewport:null,
-    });
+        const browser = await this.newBrowser();
+        const context = await browser.newContext(contextOptions)
         const page = await context.newPage();
 
         this.screen = { browser, context, page };
 
-        return this.screen;
+        return this.screen
     }
 
-    private newBrowser = async (): Promise<Browser> => { // This method is a property of ScenarioWorld class
+    private newBrowser = async (): Promise<Browser> => {
 
         const automationBrowsers = ['chromium', 'firefox', 'webkit']
-        // Using typeof with indexed access to get the type of an element in the array
-        type AutomationBrowser = typeof automationBrowsers[number] // 'chromium' | 'firefox' | 'webkit'
-        // The use of typeof automationBrowsers[number] ensures that the type of automationBrowser can only be one of the three valid browser strings. 
-        // This type safety is crucial because it restricts automationBrowser to valid inputs, 
-        // which aligns with what Playwright expects when we later use it to index playwright[automationBrowser] to get a specific BrowserType
-        const automationBrowser = env('UI_AUTOMATION_BROWSER') as AutomationBrowser;
+        type AutomationBrowser = typeof automationBrowsers[number]
+        const automationBrowser = env('UI_AUTOMATION_BROWSER') as AutomationBrowser
 
         const browserType: BrowserType = playwright[automationBrowser];
         const browser = await browserType.launch({
-            devtools: process.env.DEVTOOLS !== 'false',
             headless: process.env.HEADLESS !== 'false',
-            args: [ '---start-maximized', 
-                    '--disable-web-security', 
-                    '--disable-features=IsolateOrigins, site-per-process',
-                ],
-        });
+            devtools: process.env.DEVTOOLS !== 'false',
+            args: ['--disable-web-security', '--disable-features=IsolateOrigins, site-per-process'],
+        })
         return browser;
-    };
+    }
+
 }
 
-setWorldConstructor(ScenarioWorld);
+setWorldConstructor(ScenarioWorld)
