9346b9b811273d3922527083f36bb8690a6d8f0f
Ivan Shyshkevich <ishyshkevich@moderncampus.com>
2025-04-17 13:00:07 -0400

    aligned click.ts

--- e2e/src/step-definitions/click.ts
+++ e2e/src/step-definitions/click.ts
@@ -1,11 +1,11 @@
 import { When } from '@cucumber/cucumber';
-import { ScenarioWorld } from './setup/world';
+import { ScenarioWorld } from './setup/world'
 import {
     clickElement,
 } from '../support/html-behavior';
-import { waitFor } from '../support/wait-for-behavior';
-import { getElementLocator } from '../support/web-element-helper';
-import { ElementKey } from '../env/global';
+import { waitFor } from '../support/wait-for-behavior'
+import { getElementLocator } from "../support/web-element-helper";
+import { ElementKey } from '../env/global'
 
 When(
     /^I click the "([^"]*)" (?:button|link|icon|element)$/,
@@ -19,14 +19,15 @@ When(
 
         const elementIdentifier = getElementLocator(page, elementKey, globalConfig);
 
-        await waitFor ( async () => {
-            const result = await page.waitForSelector(elementIdentifier, {
-                state: 'visible',
-            });
-            if (result) {
-                await clickElement(page, elementIdentifier);
-            }
-            return result
-        });
+        await waitFor(async () => {
+          const result = await page.waitForSelector(elementIdentifier, {
+              state: 'visible',
+          });
+          if (result) {
+              await clickElement(page, elementIdentifier);
+          }
+          return result;
+        })
+
     }
 )
